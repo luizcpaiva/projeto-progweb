997e70c96838d48960cba593c52e31b39e59f96a
kaique-salvador <kaique.salvadr@gmail.com>
2025-10-22 13:11:48 -0300

    Configuração do Django Admin para Produtos e Categorias

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
-
-# Register your models here.
+from django.contrib import admin 
+from .models import Categoria, Produto, Pedido, ItemPedido 
+ 
+@admin.register(Categoria) 
+class CategoriaAdmin(admin.ModelAdmin): 
+    list_display = ('nome', 'slug') 
+    prepopulated_fields = {'slug': ('nome',)} 
+ 
+@admin.register(Produto) 
+class ProdutoAdmin(admin.ModelAdmin): 
+    list_display = ('nome', 'categoria', 'preco', 'estoque', 'disponivel') 
+    list_filter = ('categoria', 'disponivel') 
+    list_editable = ('preco', 'estoque', 'disponivel') 
+ 
+class ItemPedidoInline(admin.TabularInline): 
+    model = ItemPedido 
+    raw_id_fields = ['produto'] 
+    extra = 0 
+ 
+@admin.register(Pedido) 
+class PedidoAdmin(admin.ModelAdmin): 
+    list_display = ('id', 'usuario', 'status', 'criado_em') 
+    list_filter = ('status', 'criado_em') 
+    inlines = [ItemPedidoInline]
