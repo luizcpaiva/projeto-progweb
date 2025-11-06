8961ed6a9ec100ddba2047fc5de2249872388c60
kaique-salvador <kaique.salvadr@gmail.com>
2025-11-05 23:11:52 -0300

    Adiciona sistema de login/logout de usuário

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contas/', include('django.contrib.auth.urls')),
     path('', include('store.urls')),
 ]
 
